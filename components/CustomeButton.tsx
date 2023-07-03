24fe16b160422308f3d61e0fa31bbd0c10c35ef3
Yulian Melnychuk <julian95@o2.pl>
2023-07-03 15:14:33 +0200

    update

---
+++ components/CustomeButton.tsx
@@ -0,0 +1,31 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { CustomButtonProps } from '@/types';
+
+const CustomeButton = ({
+  isDisabled,
+  btnType,
+  containerStyles,
+  textStyles,
+  title,
+  rightIcon,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={isDisabled}
+      type={btnType || 'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} alt="arrow_left" fill className="object-contain" />
+        </div>
+      )}
+    </button>
+  );
+};
+
+export default CustomeButton;
---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import CustomeButton from './CustomeButton';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import CustomeFilter from './CustomeFilter';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
+import CarDetails from './CarDetails';
+import CarCard from './CarCard';
+
+export {
+  Hero,
+  CustomeButton,
+  Navbar,
+  Footer,
+  CustomeFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarDetails,
+  CarCard,
+};
